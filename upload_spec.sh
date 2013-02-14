7cb7aca2ecc9e3841a27e41ed92ba682e526cb36
Patrick Huang <pahuang@redhat.com>
2013-02-15 09:58:03 +1000

    update upload script

--- upload_spec.sh
+++ upload_spec.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+url=10.64.27.27
 
-echo will scp $1 to DropBear fedora box
+echo will scp $1 to DropBear fedora box $url
 
-scp $1 cloud-user@10.64.27.27:~/rpmbuild/SPECS
+scp $1 cloud-user@${url}:~/rpmbuild/SPECS
 
 
