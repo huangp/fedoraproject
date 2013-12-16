6e42674baea86ebfc05be25ea003c9f5e6d5954c
Patrick Huang <pahuang@redhat.com>
2013-12-16 13:13:09 +1000

    dropbear script

--- dropbear.sh
+++ dropbear.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
-url=10.64.27.27
+f17=10.64.27.27
 
-ssh cloud-user@$url
+f18=10.64.27.224
+
+f19=10.64.27.13
+
+cmd="ssh cloud-user@"
+
+if [ "$1" -eq "17" ]
+then
+    $cmd$f17
+elif [ "$1" -eq "18" ]
+then
+    $cmd$f18
+elif [ "$1" -eq "19" ]
+then
+    $cmd$f19
+fi
+  
+exit
